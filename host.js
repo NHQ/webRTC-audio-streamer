2a8a76eff693920f9b9728a348a228a9b3d68812
Tree <github@folkstack.com>
2021-08-23 17:39:30 -0400

    push

--- host.js
+++ host.js
@@ -93,11 +93,11 @@ function addMedia(id, audio=true, video=false){
           caller.on('connect', e => {
             var sink = new msrc(ael).createWriteStream(mime)
             caller.pipe(sink)
+            ael.play()
             //var src = new MediaSource()
             //ael.src = URL.createObjectURL( src )
             /*src.onsourceopen = e => {
               var srcBuf = src.addSourceBuffer(mime)
-              ael.play()
               caller.on('data', e => {
                // console.log(e)
                 srcBuf.appendBuffer(toa(e))
